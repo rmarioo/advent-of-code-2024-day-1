f28d28cbfd23287cf151a6f2be468a42e439605c
mrusso <mario.russo@lastminute.com>
2024-12-02 09:21:23 +0100

    refactor to use function composition with lodash library

--- src/historian-hysteria.js
+++ src/historian-hysteria.js
@@ -1,17 +1,21 @@
+const _ = require('lodash');
 
-function distanceList(list1_, list2_) {
-   list1= sortList(list1_);
-   list2= sortList(list2_);
-   return list1.map((value, index) => distance(value, list2[index]));
+function sortLists ({ list1, list2 })  {
+   return {
+      sortedList1: [...list1].sort(), // Sort list1 without mutating the original
+      sortedList2: [...list2].sort(), // Sort list2 without mutating the original
+   };
+};
+
+
+function distanceSorted({ sortedList1, sortedList2 }) {
+   return sortedList1.map((val, i) => Math.abs(val - sortedList2[i]));
 }
 
-function sortList(list) {
-   return list.sort((a, b) => a - b);
-}
-
-function distance(a,b) {
-   return b - a
-}
+const distanceList = _.flow(
+    sortLists,        // Step 1: Sort the lists
+    distanceSorted    // Step 2: Compute distances
+);
 
 module.exports = {
    distanceList
--- test/historian-hysteria.test.js
+++ test/historian-hysteria.test.js
@@ -5,7 +5,7 @@ test('distance between list 1,4 and list 4,9 should be list of 3,5', () => {
     let list2 = [4,9];
     let expected = [3,5];
 
-    expect(distanceList(list1, list2)).toStrictEqual(expected);
+    expect(distanceList({ list1, list2 } )).toStrictEqual(expected);
 });
 
 
@@ -14,5 +14,5 @@ test('distance between unsorted list 1,4 and list 4,9 should be list of 3,5', ()
     let list2 = [9,4];
     let expected = [3,5];
 
-    expect(distanceList(list1, list2)).toStrictEqual(expected);
+    expect(distanceList( {list1, list2 })).toStrictEqual(expected);
 });
